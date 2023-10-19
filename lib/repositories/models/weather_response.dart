2f0eecc748a1db2708198a641eedca30eb521c02
Christian Wheeler <christian@ascended.co.za>
2023-10-19 09:58:54 +0200

    Fix mapping bug

--- lib/repositories/models/weather_response.dart
+++ lib/repositories/models/weather_response.dart
@@ -156,10 +156,10 @@ class Main {
   });
 
   Main.fromJson(Map<String, dynamic> json) {
-    temp = json['temp'];
-    feelsLike = json['feels_like'];
-    tempMin = json['temp_min'];
-    tempMax = json['temp_max'];
+    temp = json['temp'] is int ? json['temp'].toDouble() : json['temp'];
+    feelsLike = json['feels_like'] is int ? json['feels_like'].toDouble() : json['feels_like'];
+    tempMin = json['temp_min'] is int ? json['temp_min'].toDouble() : json['temp_min'];
+    tempMax = json['temp_max'] is int ? json['temp_max'].toDouble() : json['temp_max'];
     pressure = json['pressure'];
     humidity = json['humidity'];
     seaLevel = json['sea_level'];
